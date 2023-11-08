4941a2e61439d6656d6e537f7dd425ef8f166c60
ErwanDuprey <erwan.duprey@free.fr>
2023-11-08 14:26:27 +0100

    Add optimized.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import autokeras as ak
 x_train = pd.read_csv("train.csv")[:25000]
 x_test = pd.read_csv("test.csv")[:25000]
 
+x_val = pd.read_csv("val.csv")
+
 if __name__ == '__main__':
     # x_train as pandas.DataFrame, y_train as pandas.Series
     y_train = x_train.pop("Etiquette_DPE")
@@ -17,6 +19,6 @@ if __name__ == '__main__':
     # Feed the structured data classifier with training data.
     clf.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test), validation_batch_size=32)
     # Predict with the best model.
-    predicted_y = clf.predict(x_test)
+    predicted_y = clf.predict(x_val)
     # Evaluate the best model with testing data.
     print(clf.evaluate(x_test, y_test))
--- optimized.py
+++ optimized.py
@@ -4,23 +4,24 @@ from keras.models import load_model
 import autokeras as ak
 import csv
 
-cols = ["Surface_habitable_desservie_par_installation_ECS", "Emission_GES_éclairage", "Conso_5_usages_é_finale_énergie_n°2", "Conso_chauffage_dépensier_installation_chauffage_n°1", "Emission_GES_chauffage_énergie_n°2", "Année_construction", "Conso_5_usages/m²_é_finale", "Conso_5_usages_é_finale","Qualité_isolation_enveloppe","Qualité_isolation_menuiseries","Qualité_isolation_murs","Qualité_isolation_plancher_bas","Qualité_isolation_plancher_haut_comble_perdu","Surface_habitable_logement","Type_bâtiment"]
+cols = ["Surface_habitable_desservie_par_installation_ECS", "Emission_GES_éclairage", "Conso_5_usages_é_finale_énergie_n°2", "Conso_chauffage_dépensier_installation_chauffage_n°1", "Emission_GES_chauffage_énergie_n°2", "Etiquette_GES", "Année_construction", "Conso_5_usages/m²_é_finale", "Conso_5_usages_é_finale","Qualité_isolation_enveloppe","Qualité_isolation_menuiseries","Qualité_isolation_murs","Qualité_isolation_plancher_bas","Qualité_isolation_plancher_haut_comble_perdu","Surface_habitable_logement","Type_bâtiment"]
+cols_with_y = ["Surface_habitable_desservie_par_installation_ECS", "Emission_GES_éclairage", "Conso_5_usages_é_finale_énergie_n°2", "Conso_chauffage_dépensier_installation_chauffage_n°1", "Emission_GES_chauffage_énergie_n°2", "Etiquette_GES", "Année_construction", "Conso_5_usages/m²_é_finale", "Conso_5_usages_é_finale","Etiquette_DPE", "Qualité_isolation_enveloppe","Qualité_isolation_menuiseries","Qualité_isolation_murs","Qualité_isolation_plancher_bas","Qualité_isolation_plancher_haut_comble_perdu","Surface_habitable_logement","Type_bâtiment"]
 
-x_train = pd.read_csv("train.csv", usecols=cols)
-x_test = pd.read_csv("test.csv", usecols=cols)
+
+x_train = pd.read_csv("train.csv", usecols=cols_with_y)
+x_test = pd.read_csv("test.csv", usecols=cols_with_y)
 x_val = pd.read_csv("val.csv", usecols=cols)
-
-y_train = pd.read_csv("train.csv", usecols=["Etiquette_DPE"])
-y_test = pd.read_csv("test.csv", usecols=["Etiquette_DPE"])
 
 x_train = x_train.dropna()
 x_test = x_test.dropna()
 
 
 if __name__ == '__main__':
+    y_train = x_train.pop("Etiquette_DPE")
+    y_test = x_test.pop("Etiquette_DPE")
+
     # It tries 10 different models.
     clf = ak.StructuredDataClassifier(overwrite=True, max_trials=3)
-    # Feed the structured data classifier with training data.
     clf.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test), validation_batch_size=32)
     # Predict with the best model.
     predicted_y = clf.predict(x_val)
