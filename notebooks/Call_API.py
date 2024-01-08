9bbf60a3a6711365dfdf689f7f3bdcc425f04061
ErwanDuprey <ErwanDuprey>
2024-01-08 21:10:38 +0000

    Call API working?

--- notebooks/Call_API.py
+++ notebooks/Call_API.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
+
 import os
 import requests
 import numpy as np
@@ -9,7 +10,7 @@ def create_tf_serving_json(data):
   return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
 
 def score_model(dataset):
-  url = 'https://adb-8468160794787762.2.azuredatabricks.net/model/API_Model/1/invocations'
+  url = 'https://adb-8468160794787762.2.azuredatabricks.net/model/To_API/1/invocations'
   headers = {'Authorization': f'Bearer {os.environ.get("DATABRICKS_TOKEN")}', 'Content-Type': 'application/json'}
   ds_dict = dataset.to_dict(orient='split') if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(dataset)
   data_json = json.dumps(ds_dict, allow_nan=True)
@@ -123,5 +124,32 @@ dataset = {
 
 # COMMAND ----------
 
-score = score_model(dataset)
+"""columns = [
+    "Emission_GES_éclairage",
+    "Conso_chauffage_dépensier_installation_chauffage_n°1",
+    "Etiquette_GES",
+    "Classe_altitude",
+    "Conso_5_usages/m²_é_finale",
+    "Conso_5_usages_é_finale",
+    "Hauteur_sous-plafond",
+    "Qualité_isolation_enveloppe",
+    "Qualité_isolation_menuiseries",
+    "Qualité_isolation_murs",
+    "Qualité_isolation_plancher_bas",
+    "Qualité_isolation_plancher_haut_comble_perdu",
+    "Surface_habitable_logement",
+    "Type_bâtiment"
+  ]
+
+# Sélectionner uniquement les colonnes spécifiées
+val_selected = val.select(*selected_cols)"""
+
+# COMMAND ----------
+
+from mlflow.models.signature import infer_signature
+import mlflow
+val = spark.read.options(inferSchema=True).table("valtable")
+val = val.toPandas()
+score = score_model(val)
 print(score)
+
